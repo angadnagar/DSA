abc8af4eb6c1dd89bd689abacbba994d435d2215
Angad Nagar <113714156+angadnagar@users.noreply.github.com>
2026-05-06 21:31:38 +0530

    Add: valid-tree - submission-2

---
+++ Data Structures & Algorithms/valid-tree/submission-2.cpp
@@ -0,0 +1,65 @@
+class Solution {
+public:
+
+    // bool dfs(int node,int parent,vector<vector<int>>& adj,vector<int>& vis){
+    //   vis[node]=1;
+
+    //   for(auto &it:adj[node]){
+    //      if(!vis[it]){
+    //       if(!dfs(it,node,adj,vis))
+    //       return false;
+    //      }
+
+    //      else if(it!=parent)
+    //      return false;
+    //   }
+
+    //   return true;
+    // }
+    bool validTree(int n, vector<vector<int>>& edges) {
+      if(edges.size()!=n-1)
+      return false;
+
+      vector<vector<int>> adj(n);
+
+      for(auto &it:edges){
+        adj[it[0]].push_back(it[1]);
+        adj[it[1]].push_back(it[0]);
+      }
+
+      vector<int> vis(n,0);
+
+      queue<pair<int,int>> q;
+      q.push({0,-1});
+
+      while(!q.empty()){
+        int node = q.front().first;
+        int parent = q.front().second;
+
+        vis[node]=1;
+        q.pop();
+
+        for(auto& it:adj[node]){
+          if(!vis[it]){
+            q.push({it,node});
+          }
+
+          else if(it!=parent){
+            return false;
+          }
+        }
+      }
+
+      // if(!dfs(0,-1,adj,vis)){
+      //   return false;
+      // }
+
+      for(auto &v:vis){
+        if(v==0)
+        return false;
+      }
+
+      return true;
+        
+    }
+};
