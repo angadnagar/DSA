ffc39a7414101755623bc3827f0af5f3d0ccc7d8
Angad Nagar <113714156+angadnagar@users.noreply.github.com>
2026-05-17 12:12:50 +0530

    Add: house-robber - submission-2

---
+++ Data Structures & Algorithms/house-robber/submission-2.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+
+    int solve(vector<int>& nums,int n,int index,vector<int>& dp){
+      if(index>=n)
+      return 0;
+
+      if(dp[index]!=-1)
+      return dp[index];
+     
+      int take1 = nums[index]+solve(nums,n,index+2,dp);
+      int take2 = solve(nums,n,index+1,dp);
+
+      return dp[index]=max(take1,take2);
+    }
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        
+        vector<int> dp(n,-1);
+
+       return solve(nums,n,0,dp);
+    }
+};
