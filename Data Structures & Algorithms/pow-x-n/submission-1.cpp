8c0b75a4eae08a1faced54035dacb019ed1ea6a3
Angad Nagar <113714156+angadnagar@users.noreply.github.com>
2026-06-10 11:48:21 +0530

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+
+    double calc(double x,int p){
+
+
+        if(p==1)
+        return x;
+
+        double half = calc(x,p/2);
+        double res = 1;
+
+        if(p%2==0)
+        res=half*half;
+
+        else
+        res=x*half*half;
+
+        return res;
+    }
+    double myPow(double x, int n) {
+        if(n==0)
+        return 1;
+
+        if(n==1)
+        return x;
+
+        int p = abs(n);
+
+        double res = calc(x,p);
+
+        return n>0 ? res : 1/res;
+    }
+};
